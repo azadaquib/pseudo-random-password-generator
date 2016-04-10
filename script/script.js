b901d0b6af42430b108ae874326eae62c1933023
azadaquib <azad.aquib@gmail.com>
2016-04-10 16:53:13 +0530

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,31 @@
+
+'use strict'
+
+import * as MersenneTwister from 'seedRandom.js'
+//  var passPhrase = document.getElementById('passphrase')
+
+var md5hash = '5d41402abc4b2a76b9719d911017c592'
+var l = md5hash.length
+var sum = 0
+document.write('<br/>' + l + '<br/>')
+var randArray = new Array(4)
+
+document.write('<br/><br/>')
+
+for (let i = 0; i < 4; i++) {
+  randArray[i] = md5hash.substring((i * 8), ((i * 8) + 8))
+  document.write((i * 8) + ' , ' + ((i * 8) + 8) + ' , ' + randArray[i] + '<br/>')
+  randArray[i] = parseInt(randArray[i], 16)
+  document.write(randArray[i] + '<br/>')
+}
+
+for (let i = 0; i < randArray.length; i++) {
+  sum += randArray[i]
+}
+
+document.write('<br/>' + sum + '<br/>')
+
+var m = new MersenneTwister(sum)
+var randomNumber = m.random()
+
+document.write('<br/>' + randomNumber + '<br/>')
